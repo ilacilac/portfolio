5cf7ea711710f3f8f8325b57f56b7d77141a9907
ilacilac <qkralswl307@gmail.com>
2021-03-22 23:21:40 +0900

    feat: Scroll event

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 const app = () => {
-  const $main = document.querySelector("main");
-  const $mainBg = document.querySelector(".main-bg");
+  const $main = document.querySelector('main');
+  const $mainBg = document.querySelector('.main-bg');
   let startX = 0;
   let startY = 0;
   let bgPosX = 0;
@@ -9,7 +9,7 @@ const app = () => {
   let movePosY = 0;
 
   $main.onmouseenter = (e) => {
-    $mainBg.style.transition = "none";
+    $mainBg.style.transition = 'none';
     // 시작 좌표
     startX = e.clientX;
     startY = e.clientY;
@@ -31,9 +31,69 @@ const app = () => {
   $main.onmouseout = (e) => {
     bgPosX = -100;
     bgPosY = -100;
-    $mainBg.style.transition = "all linear 0.3s";
+    $mainBg.style.transition = 'all linear 0.3s';
     $mainBg.style.top = `${bgPosY}px`;
     $mainBg.style.left = `${bgPosX}px`;
   };
 };
 app();
+
+const $body = document.querySelector('body');
+const $sections = document.querySelectorAll('.common-group');
+
+const windowHeight = window.innerHeight;
+
+let scrollTopDefault = 0;
+let scrollPage = 1;
+// console.log($sections);
+
+const moveSection = (e) => {
+  let { scrollTop } = e.target.scrollingElement;
+
+  $sections.forEach((section, index) => {
+    if (
+      $sections[index].offsetTop < scrollTop &&
+      $sections[index + 1].offsetTop > scrollTop
+    ) {
+      $sections[index + 1].classList.add('on');
+    } else {
+      $sections[index + 1].classList.remove('on');
+    }
+
+    
+    if (section.classList.contains('on')) {
+      console.log('asd');
+      
+      
+      window.scrollTo({
+        // top: section.offsetTop,
+        top: windowHeight * index,
+        left: 0,
+        behavior: 'smooth'
+      })
+    }
+  });
+
+  
+};
+
+const debounce = (func) => {
+  let timer;
+
+  return (...args) => {
+    // console.log('func', func); // func (e) => moveSection(e)
+    // console.log('args', args); // args [Event]
+    if (!timer) {
+      func.apply(this, args);
+    }
+
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = undefined;
+    }, 500);
+  };
+};
+
+const process = debounce((e) => moveSection(e));
+
+document.addEventListener('scroll', process);
